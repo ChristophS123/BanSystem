50a92111563c64d37419e29105150ec0483b9bf9
Christoph <schmitzchristoph7@gmail.com>
2023-06-22 10:18:23 +0200

    initial commit

---
+++ src/main/java/de/christoph/bansystem/BanSystem.java
@@ -0,0 +1,16 @@
+package de.christoph.bansystem;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BanSystem extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
